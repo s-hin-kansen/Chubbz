731fa25a3c429d6d8060d358b0ec5a3465b89419
chuanshaof <chuanshaof@gmail.com>
2023-10-27 21:36:12 +0800

    updated some lock services

--- server.go
+++ server.go
@@ -10,12 +10,11 @@ import (
 	"time"
 )
 
+// Essentially keep track of who has the lock & next in queue
 type Data struct {
 	ClientID int
 	Queue    []int
 }
-
-type Node int
 
 var (
 	isLeader bool
@@ -23,16 +22,34 @@ var (
 	dataLock sync.Mutex
 )
 
+type Node int
+
+// For Leader to handle requests from clients
 func (n *Node) HandleRequest(arg *string, reply *string) error {
 	if *arg == "REQUEST" {
+		// TODO: See how this can take in some client ID. Is it possible to get it from the RPC connection or only through the string?
+
+		// Handle case where there is a lock assigned
+		if data.ClientID != 0 {
+			// Add client to queue
+			dataLock.Lock()
+			data.Queue = append(data.Queue, 1)
+			dataLock.Unlock()
+		}
+
 		fmt.Println("leader Received request")
 		dataLock.Lock()
 		data.ClientID = 1
 		dataLock.Unlock()
 		*reply = "OK"
 	} else if *arg == "RELEASE" {
+		// Handle case of RELEASE of lock
 		dataLock.Lock()
 		data.ClientID = 0
+		if len(data.Queue) > 0 {
+			data.ClientID = data.Queue[0]
+			data.Queue = data.Queue[1:]
+		}
 		dataLock.Unlock()
 		*reply = "OK"
 	}
@@ -40,34 +57,21 @@ func (n *Node) HandleRequest(arg *string, reply *string) error {
 	return nil
 }
 
+// For Leader to send back data for replication & aliveness
 func (n *Node) ReplicateData(arg *Data, reply *Data) error {
 	dataLock.Lock()
 	defer dataLock.Unlock()
 
 	*reply = data
-	data = *arg
 
 	fmt.Printf("Leader Data: %+v\n", data)
 	return nil
 }
 
-func main() {
-	nodeID := os.Getenv("NODE_ID")
-	isLeader := nodeID == "2"
-
-	if isLeader {
-		go leaderListener()
-	} else {
-		go slavePoller()
-	}
-
-	// Block main goroutine
-	select {}
-}
-
+// Set up Leader RPC gateway for comms
 func leaderListener() {
 	node := new(Node)
-	rpc.Register(node)t
+	rpc.Register(node)
 
 	ln, err := net.Listen("tcp", ":8080")
 	if err != nil {
@@ -85,6 +89,7 @@ func leaderListener() {
 	}
 }
 
+// For Slave to poll for data update & handle simple failover procedure
 func slavePoller() {
 	for {
 		time.Sleep(5 * time.Second)
@@ -101,7 +106,7 @@ func slavePoller() {
 
 			var reply Data
 			dataLock.Lock()
-			err = client.Call("Node.ReplicateData", &data, &reply)
+			err = client.Call("Node.ReplicateData", &struct{}{}, &reply)
 			dataLock.Unlock()
 			if err != nil {
 				log.Println(err)
@@ -115,4 +120,20 @@ func slavePoller() {
 			fmt.Printf("Slave Data: %+v\n", data)
 		}
 	}
+}
+
+func main() {
+	nodeID := os.Getenv("NODE_ID")
+
+	// Set node to isLeader if nodeID is 2
+	isLeader := nodeID == "2"
+
+	if isLeader {
+		go leaderListener()
+	} else {
+		go slavePoller()
+	}
+
+	// Block main goroutine
+	select {}
 }
